4aad337b37f4dd1774c0555176698efe6ad1d173
Jake Thomas <jake@jakthom.dev>
2026-03-31 23:44:20 -0400

    refactor: rewrite sniffer to use n2k.Receive API

--- cmd/sniffer.go
+++ cmd/sniffer.go
@@ -5,48 +5,45 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"log/slog"
 	"os"
 	"os/signal"
 
-	"github.com/brutella/can"
-	"github.com/open-ships/n2k/internal/adapter"
-	"github.com/open-ships/n2k/internal/canbus"
-	"github.com/open-ships/n2k/internal/decoder"
+	"github.com/open-ships/n2k"
 )
 
-type jsonPrinter struct {
-	enc *json.Encoder
-}
-
-func (j *jsonPrinter) HandleStruct(v any) {
-	if err := j.enc.Encode(v); err != nil {
-		slog.Error(fmt.Sprintf("json encode: %v", err))
-	}
-}
-
 func main() {
-	iface := flag.String("i", "can0", "SocketCAN interface name")
+	iface := flag.String("i", "can0", "CAN interface name")
+	usb := flag.String("u", "", "USB-CAN serial port (e.g., /dev/ttyUSB0)")
+	expr := flag.String("f", "", "CEL filter expression")
+	unknown := flag.Bool("unknown", false, "include unknown PGNs")
 	flag.Parse()
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 
-	log := slog.New(slog.NewTextHandler(os.Stderr, nil))
+	var opts []n2k.Option
+	if *usb != "" {
+		opts = append(opts, n2k.USB(*usb))
+	} else {
+		opts = append(opts, n2k.CAN(*iface))
+	}
+	if *expr != "" {
+		opts = append(opts, n2k.Filter(*expr))
+	}
+	if *unknown {
+		opts = append(opts, n2k.IncludeUnknown())
+	}
+	opts = append(opts, n2k.WithLogger(slog.Default()))
 
-	printer := &jsonPrinter{enc: json.NewEncoder(os.Stdout)}
-	dec := decoder.New()
-	dec.SetOutput(printer)
-
-	a := adapter.NewCANAdapter()
-	a.SetOutput(dec)
-
-	log.Info("listening", "interface", *iface)
-	err := canbus.RunSocketCAN(ctx, log, *iface, func(frame can.Frame) {
-		a.HandleMessage(&frame)
-	})
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
+	enc := json.NewEncoder(os.Stdout)
+	for msg, err := range n2k.Receive(ctx, opts...) {
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := enc.Encode(msg); err != nil {
+			fmt.Fprintf(os.Stderr, "encode error: %v\n", err)
+		}
 	}
 }
